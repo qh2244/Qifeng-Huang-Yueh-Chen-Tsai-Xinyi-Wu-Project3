b3de88bcf832dcd727e1222d456dde4f9feffd11
Qifeng Huang <qifenghuang@Qifengs-MacBook-Air.local>
2023-12-15 15:40:27 -0800

    Update Code

---
+++ backend/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const path = require("path");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGODB,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(error => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")))
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, "public/images");
+    },
+    filename:(req,file,cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer(storage);
+app.post("api/upload",upload.single("file"), (req, res) =>{
+    try {
+        return res.status(200).json("File uploaded sucessfully.")
+    } catch (error) {
+        console.log(err)
+    }
+})
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+
+
+app.listen(8800, () => {
+    console.log("Server running...");
+});
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
+
+const INITIAL_STATE = {
+  user:JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
+
+
+export const AuthContext = createContext(INITIAL_STATE);
+
+// eslint-disable-next-line react/prop-types
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user))
+  },[state.user])
+  
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
